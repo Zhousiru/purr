821549d42397cab4c845f8c4a1d2bbeaa4e5645f
Zhousiru <i@zhousiru.com>
2024-02-06 22:06:05 +0800

    perf: model select hover & button

--- src/components/layout/model-switch/index.tsx
+++ src/components/layout/model-switch/index.tsx
@@ -23,28 +23,27 @@ const ModelSwitch = forwardRef<HTMLDivElement, ModelSwitchProps>(
       >
         <div className="flex flex-col divide-y">
           {models.map((model) => (
-            <div key={model.name} className="px-3 py-2">
-              <button
-                type="button"
-                className="flex w-full items-center justify-between gap-2 text-left"
-                onClick={() => onChange(model.name)}
-              >
-                <div>
-                  <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-                    {model.name}
-                  </div>
-                  <div className="text-sm text-gray-400">
-                    {filesize(model.size)}
-                  </div>
+            <button
+              key={model.name}
+              type="button"
+              className="flex items-center justify-between gap-2 px-3 py-2 text-left hover:bg-gray-100"
+              onClick={() => onChange(model.name)}
+            >
+              <div>
+                <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+                  {model.name}
                 </div>
-                <IconCheck
-                  className={cn(
-                    'flex-shrink-0',
-                    model.name !== value && 'hidden',
-                  )}
-                />
-              </button>
-            </div>
+                <div className="text-sm text-gray-400">
+                  {filesize(model.size)}
+                </div>
+              </div>
+              <IconCheck
+                className={cn(
+                  'flex-shrink-0',
+                  model.name !== value && 'hidden',
+                )}
+              />
+            </button>
           ))}
         </div>
       </div>
