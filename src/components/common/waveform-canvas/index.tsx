9fcdb135152b5d021070f2f3ea8cb9a503cd666b
Zhousiru <i@zhousiru.com>
2024-03-25 00:36:54 +0800

    fix: hover indicator position lags while scrolling

--- src/components/common/waveform-canvas/index.tsx
+++ src/components/common/waveform-canvas/index.tsx
@@ -67,10 +67,12 @@ export const WaveformCanvas = forwardRef<
     }
   }, [mergeChannels, path])
 
+  // Recover `scrollTop` when `path` prop changed.
   useEffect(() => {
     containerRef.current!.scrollTo({ top: 0 })
   }, [path])
 
+  // Position the current indicator.
   const currentIndicatorRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
     const unsub = player.subCurrentTime((time) => {
@@ -82,15 +84,30 @@ export const WaveformCanvas = forwardRef<
     return () => unsub()
   }, [])
 
+  // Position the fixed layer.
+  // We place hover indicator in a fixed layer to avoid lag while scrolling.
+  const hoverContainerRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const observer = new ResizeObserver(() => {
+      const rect = containerRef.current!.getBoundingClientRect()
+      hoverContainerRef.current!.style.top = rect.top + 'px'
+      hoverContainerRef.current!.style.left = rect.left + 'px'
+      hoverContainerRef.current!.style.width = rect.width + 'px'
+      hoverContainerRef.current!.style.height = rect.height + 'px'
+    })
+    observer.observe(containerRef.current!)
+
+    return () => observer.disconnect()
+  })
+
+  // Position the hover indicator.
   const hoverIndicatorRef = useRef<HTMLDivElement>(null)
   const [showHoverIndicator, setShowHoverIndicator] = useState(false)
   const handleMouseEnter = () => setShowHoverIndicator(true)
   const handleMouseLeave = () => setShowHoverIndicator(false)
   const handleMouseMove: MouseEventHandler<HTMLDivElement> = (ev) => {
     const height =
-      ev.clientY -
-      containerRef.current!.getBoundingClientRect().top +
-      containerRef.current!.scrollTop
+      ev.clientY - containerRef.current!.getBoundingClientRect().top
     hoverIndicatorRef.current!.style.top = height + 'px'
   }
 
@@ -104,6 +121,7 @@ export const WaveformCanvas = forwardRef<
     player.play()
   }
 
+  // Sync `scrollTop`.
   const isControlledScroll = useRef(false)
   useEffect(
     () =>
@@ -124,40 +142,45 @@ export const WaveformCanvas = forwardRef<
   }
 
   return (
-    <>
+    <div
+      ref={mergeRefs([containerRef, ref])}
+      className="scrollbar-none absolute inset-0 overflow-y-auto"
+      onScroll={handleContainerScroll}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseMove={handleMouseMove}
+    >
+      <canvas
+        ref={canvasRef}
+        style={{ marginBlock }}
+        className="mix-blend-multiply"
+      />
+
       <div
-        ref={mergeRefs([containerRef, ref])}
-        className="scrollbar-none absolute inset-0 overflow-y-auto"
-        onScroll={handleContainerScroll}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        onMouseMove={handleMouseMove}
-      >
-        <canvas
-          ref={canvasRef}
-          style={{ marginBlock }}
-          className="mix-blend-multiply"
-        />
-        <div
-          ref={currentIndicatorRef}
-          className="absolute inset-x-0 z-40 border-t border-blue-500"
-        />
+        ref={currentIndicatorRef}
+        className="absolute inset-x-0 z-40 border-t border-blue-500"
+      />
+
+      <div ref={hoverContainerRef} className="pointer-events-none fixed z-50">
         <div
           ref={hoverIndicatorRef}
           className={cn(
-            'absolute inset-x-0 z-50',
-            !showHoverIndicator && 'pointer-events-none opacity-0',
+            'absolute inset-x-0',
+            !showHoverIndicator && 'opacity-0',
           )}
         >
           <div className="border-t border-dashed border-blue-500" />
           <button
-            className="absolute right-6 -translate-y-1/2 rounded-md bg-blue-500 px-2 py-1"
+            className={cn(
+              'absolute right-6 -translate-y-1/2 rounded-md bg-blue-500 px-2 py-1',
+              showHoverIndicator && 'pointer-events-auto',
+            )}
             onClick={handleSeek}
           >
             <IconPlayerPlayFilled size={12} className="text-white" />
           </button>
         </div>
       </div>
-    </>
+    </div>
   )
 })
