216ad8212510ee963a3ebba73061c68f8873f887
Zhousiru <i@zhousiru.com>
2024-04-20 00:03:20 +0800

    feat: seeking of float controller

--- src/components/layout/editor/float-controller/index.tsx
+++ src/components/layout/editor/float-controller/index.tsx
@@ -2,7 +2,7 @@ import { useCurrentAudioDurationValue } from '@/atoms/editor'
 import { player } from '@/lib/player'
 import { formatSec } from '@/lib/utils/time'
 import { IconPlayerPlayFilled } from '@tabler/icons-react'
-import { useEffect, useRef, useState } from 'react'
+import { MouseEventHandler, useEffect, useRef, useState } from 'react'
 
 export function FloatController() {
   const totalDuration = useCurrentAudioDurationValue()
@@ -14,33 +14,90 @@ export function FloatController() {
 
   const [showTotalDuration, setShowTotalDuration] = useState(false)
 
+  const barRef = useRef<HTMLDivElement>(null)
   const barInnerRef = useRef<HTMLDivElement>(null)
   const knobRef = useRef<HTMLDivElement>(null)
 
+  const nowSeeking = useRef<number | null>(null)
+
+  // Update playback progress.
+  function updateBarProgress(progress: number) {
+    barInnerRef.current!.style.width = progress * 100 + '%'
+    knobRef.current!.style.right = 100 - progress * 100 + '%'
+  }
   useEffect(() => {
     const unsub = player.subCurrentTime((time) => {
-      setCurrentTime(Math.round(time))
-
-      barInnerRef.current!.style.width = (time / totalDuration) * 100 + '%'
-      knobRef.current!.style.right = 100 - (time / totalDuration) * 100 + '%'
+      if (nowSeeking.current === null) {
+        setCurrentTime(Math.round(time))
+        updateBarProgress(time / totalDuration)
+      }
     })
     return () => unsub()
   }, [totalDuration])
 
+  // Handle seek.
+  function calcSeekingProgress(clientX: number) {
+    let left = clientX - barRef.current!.getBoundingClientRect().left
+    if (left < 0) {
+      left = 0
+    }
+    if (left > barRef.current!.offsetWidth) {
+      left = barRef.current!.offsetWidth
+    }
+
+    return left / barRef.current!.offsetWidth
+  }
+
+  useEffect(() => {
+    const seekMouseMove = (e: MouseEvent) => {
+      if (nowSeeking.current === null) return
+      nowSeeking.current = calcSeekingProgress(e.clientX)
+      setCurrentTime(totalDuration * nowSeeking.current)
+      updateBarProgress(nowSeeking.current!)
+    }
+
+    const seekMouseUp = () => {
+      if (nowSeeking.current === null) return
+      console.log('FloatController.Seek', nowSeeking.current)
+      player.seek(totalDuration * nowSeeking.current)
+      nowSeeking.current = null
+    }
+
+    document.addEventListener('mousemove', seekMouseMove)
+    document.addEventListener('mouseup', seekMouseUp)
+
+    return () => {
+      document.removeEventListener('mousemove', seekMouseMove)
+      document.removeEventListener('mouseup', seekMouseUp)
+    }
+  }, [totalDuration])
+
+  const handleSeekMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+    nowSeeking.current = calcSeekingProgress(e.clientX)
+    setCurrentTime(totalDuration * nowSeeking.current)
+    updateBarProgress(nowSeeking.current)
+  }
+
   return (
     <div className="pointer-events-none absolute inset-x-0 bottom-10 flex justify-center">
       <div className="pointer-events-auto rounded-lg border bg-gray-900/50 shadow-lg backdrop-blur backdrop-saturate-150">
-        <div className="group mx-2 mt-2 flex h-4 items-center px-2">
+        <div
+          className="group mx-2 mt-2 flex h-4 cursor-pointer items-center px-2"
+          onMouseDown={handleSeekMouseDown}
+        >
           <div className="relative">
-            <div className="relative h-0.5 w-[350px] overflow-hidden rounded-full bg-white/25 transition-all duration-100 group-hover:h-1">
+            <div
+              className="relative h-0.5 w-[350px] overflow-hidden rounded-full bg-white/25 group-hover:h-1"
+              ref={barRef}
+            >
               <div
                 ref={barInnerRef}
-                className="absolute inset-y-0 left-0 bg-white transition-all duration-100"
+                className="pointer-events-none absolute inset-y-0 left-0 bg-white"
               />
             </div>
             <div
               ref={knobRef}
-              className="absolute top-1/2 h-2.5 w-2.5 -translate-y-1/2 translate-x-1/2 rounded-full bg-white shadow transition-all duration-100 group-hover:h-3 group-hover:w-3"
+              className="pointer-events-none absolute top-1/2 h-2.5 w-2.5 -translate-y-1/2 translate-x-1/2 rounded-full bg-white shadow group-hover:h-3 group-hover:w-3"
             />
           </div>
         </div>
